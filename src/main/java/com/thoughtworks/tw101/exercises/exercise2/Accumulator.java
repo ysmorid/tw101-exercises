9982c2fe63b9baa787cf0cb1a75d47aff3e85918
Ylia Morid <Ylia Morid>
2016-12-13 22:12:50 -0800

    increment method increments int counter every time it is called. Total displays counter value when called.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
+// Exercise #2: Create a class that counts how many times you call the method increment() and prints out that number
+// when you call total(). Create a main method that calls increment() 5 times and then calls total().
+
 public class Accumulator {
+   private int counter = 0;
+
+   public Accumulator(){
+   }
+
+   public void increment(){
+      counter++;
+   }
+
+   public void total(){
+      System.out.println("You have called increment " + counter + " times.");
+   }
 }
