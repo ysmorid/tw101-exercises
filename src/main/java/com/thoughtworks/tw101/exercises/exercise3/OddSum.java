4b65fb0216c72480ec72bad9d4b8e871c2cb7607
Ylia Morid <Ylia Morid>
2016-12-14 02:40:17 -0800

    of() method uses a foor loop to sum the odd numbers in a given range, then returns the sum.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,8 +1,17 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
+   private int sum = 0;
 
-    public int of(int start, int end) {
-        return -1;
+   public int of(int start, int end) {
+       int num = start;
+
+       for(int i = start; i <= end; i++){
+          if(num % 2 != 0){
+             sum += num;
+          }
+          num++;
+       }
+       return sum;
     }
 }
