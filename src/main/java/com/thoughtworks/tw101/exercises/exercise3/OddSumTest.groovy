c0b0a9ccabcc77cfbc785caf45f7b4485adc9458
Ylia Morid <ysmorid@users.noreply.github.com>
2016-12-18 15:17:29 -0800

    updated jUnit test for exercise 3, deleted .groovy test class

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSumTest.groovy
+++
@@ -1,8 +0,0 @@
-package com.thoughtworks.tw101.exercises.exercise3
-
-class OddSumTest extends GroovyTestCase {
-    void testOf() {
-        OddSum test = new OddSum();
-        assertEquals(25, test.of(1, 10));
-    }
-}
--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSumTest.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSumTest.java
@@ -3,6 +3,5 @@ package com.thoughtworks.tw101.exercises.exercise3;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OddSumTest {
-    OddSum test = new OddSum();
-    assertEquals(25, test.of(1, 10));
+
 }
