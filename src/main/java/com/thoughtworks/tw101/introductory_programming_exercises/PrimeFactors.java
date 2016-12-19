0b00325ea541ac3fe4f1420bfe54032bdf55e00d
Ylia Morid <ysmorid@users.noreply.github.com>
2016-12-19 03:43:25 -0800

    determines prime factors of a given number by factorization, updating the number through iterations until all prime factors are found.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,34 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        System.out.print(primeFactors);
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> prime = new ArrayList<>();
+        int num = n;
+        int j = 0;
+        int counter = 2;
+
+        for(int i = 0; i < num; i++){
+            if(num % counter == 0){
+                prime.add(counter);
+                num /= counter;
+                j++;
+            }
+            else
+                counter++;
+        }
+        if(num != 1) {
+            prime.add(num);
+        }
+
+        return prime;
     }
 }
