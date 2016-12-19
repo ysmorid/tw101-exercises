d6e5c9a9ca96c53518b0eed77feb4f54c12eb0ff
Ylia Morid <Ylia Morid>
2016-12-18 17:24:01 -0800

    Formula to calculate average area of all rectangles in array rectangles[], then returns result.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,13 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int area = 0;
+        int average;
+
+        for(int i = 0; i < rectangles.length; i++){
+            area += rectangles[i].area();
+        }
+        average = area/rectangles.length;
+        return average;
     }
 }
