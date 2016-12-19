50d3a8e461304c168b6f45a1b31f016c67c355c4
Ylia Morid <ysmorid@users.noreply.github.com>
2016-12-19 04:12:01 -0800

    Completed triangle exercises

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/TriangleExercises.java
@@ -12,14 +12,19 @@ public class TriangleExercises {
 //    Print one asterisk to the console.
 //    Example: *
     private static void drawAnAsterisk() {
-
+        System.out.println("*");
+        System.out.println();
     }
+
 
 //    Draw a horizontal line
 //    Given a number n, print n asterisks on one line.
 //    Example when n=8:  ********
     private static void drawAHorizontalLine(int n) {
-
+        for(int i = 0; i < n; i++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
 
 //    Draw a vertical line
@@ -29,7 +34,10 @@ public class TriangleExercises {
 //            *
 //            *
     private static void drawAVerticalLine(int n) {
-
+        for(int i = 0; i < n; i++){
+            System.out.println("*");
+        }
+        System.out.println();
     }
 
 //    Draw a right triangle
@@ -39,6 +47,14 @@ public class TriangleExercises {
 //            **
 //            ***
     private static void drawARightTriangle(int n) {
+        int counter = 1;
+        for (int i = 0; i < n; i++){
+            for(int j = 0; j < counter; j++){
+                System.out.print("*");
+            }
+            counter++;
+            System.out.println();
+        }
 
     }
 }
