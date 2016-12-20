15bd8ef4473ceb518d24eee9c97393d30f42d4ea
Ylia Morid <ysmorid@users.noreply.github.com>
2016-12-20 03:20:29 -0800

    removed constructor and getter; replaced with generateRandomNumber().

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Randomizer.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Randomizer.java
@@ -4,16 +4,10 @@ import java.util.Random;
 
 public class Randomizer {
     private Random random = new Random();
-    private int number = 0;
     private final int min = 1;
     private final int max = 100;
 
-    public void getRandomNumber(){
-    number = random.nextInt((max-min) + 1) + min;
-    }
-
-    public void displayRandomNumber(){
-        getRandomNumber();
-        System.out.println(number);
+    public int generateRandomNumber(){
+        return random.nextInt((max-min) + 1) + min;
     }
 }
