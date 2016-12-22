e70506a2d713c9422421385c8c06fb381227d1b2
Ylia Morid <ysmorid@users.noreply.github.com>
2016-12-22 01:17:18 -0800

    addGuess() adds all valid guesses to guesses<>() ArrayList. displayGuesses() displays contents of guesses.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Guesses.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Guesses.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.tw101.exercises.exercise8;
 
+import java.util.ArrayList;
+
 public class Guesses {
+    private ArrayList<Integer> guesses = new ArrayList<>();
+
+    public void addGuess(int guess){
+        guesses.add(guess);
+    }
+    
+    public void displayGuesses(){
+        System.out.println("Here are all of your guesses: ");
+        for (int guess : guesses) {
+            System.out.print(guess + ", ");
+        }
+    }
+
 }
