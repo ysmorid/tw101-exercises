307c7fb00e07aa6e6e4e68a394d2433d618f8907
Ylia Morid <Ylia Morid>
2016-12-30 17:27:56 -0800

    added recursive if/else if statements in add() method to add new nodes to Binary Tree.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -2,17 +2,50 @@ package com.thoughtworks.tw101.exercises.exercise9;
 
 import java.util.List;
 
+// Exercise #9: Create a class Node that implements a binary tree for Strings. It should have an add(String name) method
+// that recursively finds the correct place to add a new node and a names() method that returns the names of all of the
+// nodes in alphabetical order (this should also be implemented using recursion).
+
+
 public class Node {
     private String name;
+    private Node parent;
+    private Node leftChild;
+    private Node rightChild;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        if (parent == null) {
+            parent = new Node(nameOfNewNode);
+        }
+        else if (nameOfNewNode.compareTo(parent.name()) < 0){
+            if (leftChild == null) {
+                leftChild = new Node(nameOfNewNode);
+            }
+            else{
+                parent = leftChild;
+                parent.add(nameOfNewNode);
+            }
+        }
+        else if (nameOfNewNode.compareTo(parent.name()) > 0){
+            if (rightChild == null) {
+                rightChild = new Node(nameOfNewNode);
+            }
+            else{
+                parent = rightChild;
+                parent.add(nameOfNewNode);
+            }
+        }
     }
 
-    public List<String> names() {
+    public List<String> names(){
         return null;
+    }
+
+    public String name(){
+        return name;
     }
 }
