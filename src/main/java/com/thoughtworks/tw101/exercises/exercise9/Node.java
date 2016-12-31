8ba18a99b8a6392e6304cdfd38a743c6a1dbd063
Ylia Morid <Ylia Morid>
2016-12-31 05:14:06 -0800

    removed comments

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -3,11 +3,6 @@ package com.thoughtworks.tw101.exercises.exercise9;
 import java.util.ArrayList;
 import java.util.List;
 
-// Exercise #9: Create a class Node that implements a binary tree for Strings. It should have an add(String name) method
-// that recursively finds the correct place to add a new node and a names() method that returns the names of all of the
-// nodes in alphabetical order (this should also be implemented using recursion).
-
-
 public class Node {
     private String name;
     private Node leftChild;
