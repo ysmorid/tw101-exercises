c73956a69e829e9db8b163deaedb7499cf515ab8
Ylia Morid <Ylia Morid>
2017-01-03 17:58:23 -0800

    updated parameters for isWithinRange() and checkGuess() to receive integer

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Results.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Results.java
@@ -3,22 +3,21 @@ package com.thoughtworks.tw101.exercises.exercise8;
 public class Results {
     private UserInput input;
     private int randomNumber;
-    private int inputAsInt;
 
     public Results(UserInput input, int randomNumber) {
         this.input = input;
         this.randomNumber = randomNumber;
-        inputAsInt = input.stringToInteger();
     }
 
-    private void isWithinRange() {
+    private void isWithinRange(int inputAsInt) {
         while (inputAsInt < 1 || inputAsInt > 100) {
             System.out.println("I'm sorry, that is an invalid number.  Please try again. ");
             input.getInput();
+            inputAsInt = input.stringToInteger();
         }
     }
 
-    protected void checkGuess(){
+    public void checkGuess(int inputAsInt){
         if(inputAsInt > randomNumber){
             System.out.println("I'm sorry.  Your guess is too high.  Guess again!");
         }
@@ -29,10 +28,10 @@ public class Results {
             System.out.println("You guessed correctly! Congratulations!!");
     }
 
-    protected void checkInputIsValid(){
+    public void checkInputIsValid(){
         try{
             input.getInput();
-            isWithinRange();
+            isWithinRange(input.stringToInteger());
         }
         catch (NumberFormatException e) {
             System.out.println("I'm sorry, that is an invalid number format. Please try again using digits only.");
